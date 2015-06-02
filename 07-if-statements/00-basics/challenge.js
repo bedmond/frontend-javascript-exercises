114510177c112067e004a32309a30d790eea9602
vagrant <vagrant@precise32.(none)>
2015-06-02 00:39:54 +0000

    07-if-statements completed

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,38 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess) {
+if (fav < guess) {
+  return "Too high";
+}
+else if (fav > guess) {
+  return "Too low";
+}
+else if (fav === guess) {
+  return "You got it!";
+}
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(first, second, third, fourth) {
+if ((first === 3 || first === 5 || first === 7) && second === 2 && (third >= 5 && third <= 100) && (fourth < 9 || fourth > 20)) {
+  return "correct";
+}
+else {
+  return "incorrect";
+}
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+if (item == 'MacBook Air' && money >= 999) {
+  return true;
+}
+else if (item == 'MacBook Pro' && money >= 1299) {
+  return true;
+}
+else if (item == 'Mac Pro' && money >= 2499) {
+  return true;
+}
+else if (item == 'Apple Sticker' && money >= 1) {
+  return true;
+}
+else {
+  return false;
+}
+};
