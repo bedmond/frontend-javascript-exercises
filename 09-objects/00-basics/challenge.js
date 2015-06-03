84780a0004fea21e2e92a1e66f9c3cdd9a3a76c0
vagrant <vagrant@precise32.(none)>
2015-06-03 23:44:18 +0000

    09-objects completed

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,32 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  }
+  return course;
+};
 
-module.exports.addProperty = undefined;
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (object[newProp] === undefined) {
+    object[newProp] = newValue;
+  }
+  return object;
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(b) {
+  return "Hello " + b.recipient + ",\n\n" + b.msg + "\n\nSincerely,\n" + b.sender;
+};
+
+module.exports.canIGet = function(item, money) {
+  var merch = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+};
+
+price = merch[item]
+return (price !== undefined && money >= price);
+};
