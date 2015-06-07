ea896c6b087d13f9ca556e68086b7b016cf3b3f9
vagrant <vagrant@precise32.(none)>
2015-06-07 15:27:10 +0000

    completed 10-loops

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object) {
+  var output = [];
+  for (var key in object) {
+    output.push(key);
+  }
+  return output;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object) {
+  var output = [];
+    for (var val in object) {
+      output.unshift(object[val]);
+    }
+    return output;
+  };
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object) {
+  var output = [];
+  for (var str in object) {
+    output.push(str + " is " + object[str]);
+  }
+  return output;
+};
--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,23 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object) {
+  var output = {};
+  for (var obj in object) {
+    output[obj] = object[obj];
+  }
+  return output;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+  for (var obj in src) {
+    dest[obj] = src[obj];
+  }
+  return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, elem) {
+  for (var i = 0; i < elem.length; i++) {
+    if (object[elem[i]] === undefined) {
+      return false;
+    }
+  }
+  return true;
+};
